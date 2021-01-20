e8aea7b33ca669b79a1950b7f364f3cdbc44be95
Nacho <ignacio5garcia@hotmail.com>
2021-01-19 23:47:44 -0300

    agregado client

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import onlineIcon from '../../icons/onlineIcon.png';
+import Github from '../Github/Github';
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+    <div className="textContainer">
+        <div>
+            <h1>Chat Application</h1>
+        </div>
+        {users ? (
+        <div>
+            <h1>People currently chatting:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+        </div>
+    ) : null}
+    <Github />
+    </div>
+)
+
+export default TextContainer;
